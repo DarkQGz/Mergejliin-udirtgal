fec3b7ac7a79df5e1e87c612fcec1d93d448b639
DarkQG <sw23d001@mandakh.edu.mn>
2023-11-16 21:50:05 +0800

    Update na6.py

--- 11/na6.py
+++ 11/na6.py
@@ -1,15 +1,6 @@
 import time
-n = int(input())
-s = time.time()
+n, s = int(input()), time.time()
 for a in range(2, n + 1):
-    d = 1  
-    for i in range(2, int(a**0.5) + 1):
-        if a % i == 0:
-            d += i
-            if i != a // i:  
-                d += a // i
-    if a == d:
-        print(a)
-e = time.time()
-t = e - s
-print(f"{t:.2f}")
+    d = sum(i + a // i for i in range(2, int(a**0.5) + 1) if a % i == 0) + 1
+    if a == d: print(a)
+print(f"{time.time() - s:.2f}")
