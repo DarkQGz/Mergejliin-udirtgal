e22ad5a236572728237845ed8b17506bb1ba4406
DarkQG <sw23d001@mandakh.edu.mn>
2023-11-16 21:57:53 +0800

    Update na1.py

--- 11/na1.py
+++ 11/na1.py
@@ -1,8 +1,1 @@
-for num in range(2, 10001):
-    a = True
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            a = False
-            break
-    if a:
-        print(num)
+print(*[num for num in range(2, 10001) if all(num % i for i in range(2, int(num**0.5) + 1))], sep='\n')
