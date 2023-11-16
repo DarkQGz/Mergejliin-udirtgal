cb6eda21dbc008e927c75da5806ea62688eebd42
DarkQG <sw23d001@mandakh.edu.mn>
2023-11-16 21:45:35 +0800

    Create na5.py

---
+++ 11/na5.py
@@ -0,0 +1,10 @@
+n = int(input())
+for a in range(2, n + 1):
+    d = 1
+    for i in range(2, int(a**0.5) + 1):
+        if a % i == 0:
+            d += i
+            if i != a // i:
+                d += a // i
+    if a == d:
+        print(a)
