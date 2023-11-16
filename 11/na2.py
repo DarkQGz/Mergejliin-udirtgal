e5419e8d4b21ce053122153fd0947d444d738a44
DarkQG <sw23d001@mandakh.edu.mn>
2023-11-16 21:57:16 +0800

    Update na2.py

--- 11/na2.py
+++ 11/na2.py
@@ -1,9 +1,2 @@
-n = int(input())
-for num in range(2, n + 1):
-    a = True
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            a = False
-            break
-    if a:
-        print(num)
+n=int(input())
+print(*[num for num in range(2, n + 1) if all(num % i for i in range(2, int(num**0.5) + 1))], sep='\n')
