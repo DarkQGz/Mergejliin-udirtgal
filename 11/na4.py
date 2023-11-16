5d4005afb8b80698510339f4d68b7b530246959a
DarkQG <sw23d001@mandakh.edu.mn>
2023-11-16 21:52:38 +0800

    Update na4.py

--- 11/na4.py
+++ 11/na4.py
@@ -1,9 +1,3 @@
 for a in range(2, 10001):
-    d = 1  
-    for i in range(2, int(a**0.5) + 1):
-        if a % i == 0:
-            d += i
-            if i != a // i:  
-                d += a // i
-    if a == d:
-        print(a)
+    d = 1 + sum(i + a // i for i in range(2, int(a**0.5) + 1) if a % i == 0 and i != a // i)
+    if a == d: print(a)
