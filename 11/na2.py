200922f5f26cbe465c349385e1ecf167c5920c49
DarkQG <sw23d001@mandakh.edu.mn>
2023-11-16 21:38:01 +0800

    Create na2.py

---
+++ 11/na2.py
@@ -0,0 +1,9 @@
+n = int(input())
+for num in range(2, n + 1):
+    a = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            a = False
+            break
+    if a:
+        print(num)
