b2d2b3381036e6af15610f47b6c4f1dce1eb2fb4
DarkQG <sw23d001@mandakh.edu.mn>
2023-11-16 21:56:26 +0800

    Update na3.py

--- 11/na3.py
+++ 11/na3.py
@@ -1,14 +1,4 @@
 import time
-n = int(input())
-s = time.time()
-for num in range(2, n + 1):
-    a = True
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            a = False
-            break
-    if a:
-        print(num)
-g = time.time()
-e = g - s
-print(f"{e:.2f}")
+n, s = int(input()), time.time()
+print(*[num for num in range(2, n + 1) if all(num % i for i in range(2, int(num**0.5) + 1))], sep='\n')
+print(f"{time.time() - s:.2f}")
