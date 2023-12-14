65ecc2f8e2546ba752f549cba6f805a0369a3bcf
DarkQG <sw23d001@mandakh.edu.mn>
2023-12-14 22:16:03 +0800

    Update 14

---
+++ 14/DefUrjSum.py
@@ -0,0 +1,19 @@
+n = int (input())
+
+nlist = list (map (int, input().split()
+
+K = int(input())
+
+klist = list (maplint, input()splitis)
+
+S = int (input())
+
+5 list = list(map (int, input().split())
+
+sums=0
+
+for i in rangdo, n, 1): sums = sums+nlist[i]
+
+urj=1
+
+for i in range (0,K, 1): urj=urj ✱ klist[i]
