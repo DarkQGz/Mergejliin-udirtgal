b2496c2d55bded4897d40006ca3070ec86a163a5
DarkQG <sw23d001@mandakh.edu.mn>
2023-12-14 22:09:42 +0800

    Create Suum.py

---
+++ 14/Suum.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("suum.in", "r")
+sys.stdout = open("suum.out", "w")
+n = int(input())
+nlist= list(map(int, input().split()))
+o = int(input())
+olist= list(map(int, input().split()))
+s = int(input())
+slist= list(map(int, input().split()))
+sums = 0
+for i in range(0, n, 1):
+    sums+=nlist[i]
+urj = 1
+for i in range(0, o, 1):
+    urj*=olist[i]
+max1 = max(slist)
+print(sums)
+print(urj)
+print(max1)
