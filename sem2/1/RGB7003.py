a5be2df1cbd7f653142bc3d890e102d7493f5797
DarkQG <sw23d001@mandakh.edu.mn>
2024-01-29 11:16:22 +0800

    Update RGB7003.py

--- sem2/1/RGB7003.py
+++ sem2/1/RGB7003.py
@@ -1,10 +1,10 @@
-a = int(input())
-def func(x):
-    result = x ** 3
+a,b = map(int,input().split())
+def func(x,y):
+    result = x * y
     return result
-def funcc(x):
-    resul = (x ** 2) * 6
+def funcc(x,y):
+    resul = 2 *(x + y)
     return resul
-n = func(a)
-b = funcc(a)
-print(n,b)
+n = func(a,b)
+c = funcc(a,b)
+print(n,c)
